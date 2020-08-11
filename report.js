3d65deaa871dcabe2596c76673f43079b361136b
s1632160 <63491245+s1632160@users.noreply.github.com>
2020-08-11 15:15:31 +0900

    Update report.js

--- report.js
+++ report.js
@@ -51,7 +51,7 @@ class Keisan {
       let b = 1.0;
       let c;
       
-      target.insertAdjacentHTML('beforeend', "<br>x^3 + x - 1 の2分法による数値計算<br>");
+      target.insertAdjacentHTML('beforeend', "<br><br>x^3 + x - 1 の2分法による数値計算<br>");
       target.insertAdjacentHTML('beforeend', "初期値 a=" + a + "<br>");
       target.insertAdjacentHTML('beforeend', "初期値 b=" + b + "<br>");
 
@@ -66,7 +66,7 @@ class Keisan {
       let a = 1.0;
       let b;
 
-      target.insertAdjacentHTML('beforeend', "<br>x^3 + x - 1 のニュートン法による数値計算<br>");
+      target.insertAdjacentHTML('beforeend', "<br><br>x^3 + x - 1 のニュートン法による数値計算<br>");
       target.insertAdjacentHTML('beforeend', "初期値 a=" + a + "<br>");
 
       while (1) {
